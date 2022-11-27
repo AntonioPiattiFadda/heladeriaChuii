e26b94b32081fd26014b73ee09cf9142dc0e8d19
Antonio Piatti Fadda <antonio.piattifadda@gmail.com>
2022-11-27 12:19:10 -0300

    Footer y Navbar terminadas y pagina de contacto terminada, Solo queda lo de textarea que no lo tengo definido

---
+++ assets/imagenes.js
@@ -0,0 +1,6 @@
+const logoChuiiFondoClaro = './LOGO_CHUII_fondo-claro.png';
+const bars = './icons8-bars-24.png';
+const facebookIcon= './icons8-facebook-48.png';
+const instagramIcon = './icons8-instagram-48.png';
+
+export default imagenes;
